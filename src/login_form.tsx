e401edccee2c02360c278bff1f153d9621f1e5a6
Simon Lukasik <simon.lukasik@crowdstrike.com>
2022-04-14 14:26:52 +0100

    Initial sketches for the log-in

--- src/login_form.tsx
+++ src/login_form.tsx
@@ -1,7 +1,51 @@
 import React from "react";
+import MenuItem from "@mui/material/MenuItem";
+import Select, { SelectChangeEvent } from "@mui/material/Select";
+import {
+  Button,
+  FormControl,
+  InputLabel,
+  Input,
+  Typography,
+  FormHelperText,
+} from "@mui/material";
 
 export class LoginForm extends React.PureComponent {
+  handleChange() {
+    console.log("asdf");
+  }
   render() {
+    const age = 20;
+    return (
+      <>
+        <Typography variant="h5">API Log-in</Typography>
+
+        <FormControl>
+          <InputLabel>CrowdStrike Cloud Region</InputLabel>
+          <Select
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            value={age}
+            label="CrowdStrike Cloud Region"
+            onChange={this.handleChange}
+          >
+            <MenuItem value={10}>Ten</MenuItem>
+            <MenuItem value={20}>Twenty</MenuItem>
+            <MenuItem value={30}>Thirty</MenuItem>
+          </Select>
+        </FormControl>
+        <FormControl>
+          <InputLabel htmlFor="my-input">Email address</InputLabel>
+          <Input id="my-input" aria-describedby="my-helper-text" />
+          <FormHelperText id="my-helper-text">
+            We'll never share your email.
+          </FormHelperText>
+        </FormControl>
+        <Button variant="contained">Log-in</Button>
+      </>
+    );
+  }
+  renders() {
     return (
       <form id="loginForm">
         <h2>CrowdStrike API: Please Log in</h2>
--- src/popup.tsx
+++ src/popup.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Paper from "@mui/material/Paper";
 import { Manager } from "./falcon";
 import { LoginForm } from "./login_form";
+import CssBaseline from "@mui/material/CssBaseline";
+import Container from "@mui/material/Container";
+import { Box, Typography } from "@mui/material";
 
 interface PopupProps {
   loggedIn: boolean;
@@ -20,7 +24,24 @@ class CrwdPopup extends React.PureComponent<PopupProps, {}> {
 
 ReactDOM.render(
   <React.StrictMode>
-    <CrwdPopup loggedIn={Manager.getInstance().loggedIn()} />
+    <Container sx={{ minWidth: "600px", minHeight: "250px" }}>
+      <CssBaseline />
+      <Paper
+        style={{
+          display: "grid",
+          gridRowGap: "20px",
+          padding: "20px",
+          margin: "10px 20px",
+        }}
+      >
+        <Typography variant="h4">
+          CrowdStrike Falcon
+          <Box component="img" src="/icons/icon128.png" />
+        </Typography>
+
+        <CrwdPopup loggedIn={Manager.getInstance().loggedIn()} />
+      </Paper>
+    </Container>
   </React.StrictMode>,
   document.getElementById("root")
 );
